651fd4c04c04fddc5522e43910402cebc2949dc6
Mohsin <92655551+mohsinss@users.noreply.github.com>
2021-10-24 19:01:39 -0700

    now

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const cheerio = require('cheerio');
 const express = require('express');
 
 const  app = express()
-app.listen(PORT, () => console.log('server running on PORT ${PORT}') )
+app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
