f3943a69173591bc52cf3fbdee1d7ab32585f020
Mohsin <92655551+mohsinss@users.noreply.github.com>
2021-10-26 21:06:33 -0700

    the gardian

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 const  app = express()
 
-const url = 'https://www.aljazeera.com/'
+const url = 'https://www.theguardian.com/us'
 
 axios(url)
     .then(response => {
