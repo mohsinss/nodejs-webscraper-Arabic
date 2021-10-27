89a1027b5cd72c35314b57032a7f3ae586d0eb94
Mohsin <92655551+mohsinss@users.noreply.github.com>
2021-10-26 20:41:09 -0700

    scraper

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 
-const  app = express()
+const  app1 = express()
 
 const url = 'https://www.theguardian.com/us'
 
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
-    })
+        const $ = cheerio.load(html)
+        const articles = []
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+
+
+        })
+        console.log(articles)
+    }) .catch(err => console.log(err))
+
+app1.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
