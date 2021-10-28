d5d098fd9c973146a20cc8501e680cc229f8a76e
Mohsin <92655551+mohsinss@users.noreply.github.com>
2021-10-27 18:49:58 -0700

    alarabia

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 const  app = express()
 
-const url = 'https://www.aljazeera.net/'
+const url = 'https://www.alarabiya.net/'
 
 axios(url)
     .then(response => {
@@ -13,7 +13,7 @@ axios(url)
         const $ = cheerio.load(html)
         const articles = []
 
-        $('.article-card__title', html).each(function() {
+        $('.sectionHero_title', html).each(function() {
             const title = $(this).text()
             const url = $(this).find('a').attr('href')
             articles.push({
