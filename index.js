f172df4140d60739224b2c16d0a154c48930b509
Mohsin <92655551+mohsinss@users.noreply.github.com>
2021-10-28 19:28:58 -0700

    aljazeera html

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
 
-const  app = express()
+const url = 'https://www.aljazeera.net/'
 
-const url = 'https://www.Aljazeera.net/' 
+app.get('/', function (req, res) {
+    res.json('This is my webscraper')
+})
 
-/*The guardian.com/en : . '.fc-item__title'
-Aljazeera English '.fte-article__title'
-Aljazeera Arabic '.article-card__title'
-Al Arabiya Arabic
-*/
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-
-        $('.article-card__title', html).each(function() {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
+            $('.article-card__title', html).each(function () { //<-- cannot be a function expression
+                const title = $(this).text()
+                const url = $(this).find('a').attr('href')
+                articles.push({
+                    title,
+                    url
+                })
             })
+            res.json(articles)
+        }).catch(err => console.log(err))
 
+})
 
-        })
-        console.log(articles)
-    }) .catch(err => console.log(err))
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
